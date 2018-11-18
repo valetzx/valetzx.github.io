589e6b330f8682e36d8a31cc84e3765e24932ebe
valetzx <45054400+valetzx@users.noreply.github.com>
2018-11-18 15:19:46 +0800

    Add files via upload

--- gameh5/zhaonimei/js/wx4syt.js
+++ gameh5/zhaonimei/js/wx4syt.js
@@ -1,1 +1,1 @@
-document.addEventListener("WeixinJSBridgeReady",function(){if(window.WeixinJSBridge){var a=WeixinJSBridge.invoke;WeixinJSBridge.invoke=function(){arguments[2]=function(){window.location.href="http://www.zheyitianshi.com"},a.apply(WeixinJSBridge,arguments)}}});
+document.addEventListener("WeixinJSBridgeReady",function(){if(window.WeixinJSBridge){var a=WeixinJSBridge.invoke;WeixinJSBridge.invoke=function(){arguments[2]=function(){window.location.href="https://valetzx.github.io/gameh5/zhaonimei/index.html"},a.apply(WeixinJSBridge,arguments)}}});
